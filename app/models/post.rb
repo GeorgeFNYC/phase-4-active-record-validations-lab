921619f891103c79c16da5c4b0b291068067725e
George Fernandez <fernandezg704@gmail.com>
2022-11-03 16:47:01 -0400

    commit tree 5580552a6795a2dccf2681367852db7189bc4f5f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_bait
+    # validate :title_bait?
+
+
+    # TITLE_BAIT = [
+    #     /Won't Believe/i,
+    #     /Secrets/i,
+    #     /Top \d/i,
+    #     /Guess/i
+    # ]
+
+    # def title_bait?
+    #     if TITLE_BAIT.none? {|pat| pat.match title}
+    #         errors.add(:title, "Must be clickbait")
+    #     end
+    # end
+
+
+    def title_bait
+        if title === "True Facts"
+            errors.add(:title, "Nope won't allow it")
+        end
+    end
 end
